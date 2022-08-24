a603690ff88986aba1512a7b358bba5abc52b745
Ludwig Stumpp <42147848+LudwigStumpp@users.noreply.github.com>
2022-08-24 17:56:46 +0200

    Changed baudrate from 9600 to 57600

--- src/utils.h
+++ src/utils.h
@@ -4,7 +4,7 @@
 #define INTERNAL_OSC_PERIOD_MICROSECS 0.0375
 #define INTERNAL_OSC_FREQ_HZ 26.667e6
 
-#define BAUDRATE 9600
+#define BAUDRATE 57600
 #define CONTROL_FREQUENCY 0.01
 
 float convertRanges(float aMin, float aMax, float bMin, float bMax, float aVal);
